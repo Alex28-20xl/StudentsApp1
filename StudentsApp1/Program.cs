dbaa3e9d9733dfcf1aae5097cfe040ac508f424b
Alexsandr <griefgiver@yandex.ru>
2022-01-21 04:13:22 +0300

    добавлены реализации команд: clear(очистка экрана), selectall(вывести всё), sortby(сортировка по имени или дате рождения в возрастающем - asc- или убывающем -desc- порядке)

--- StudentsApp1/Program.cs
+++ StudentsApp1/Program.cs
@@ -9,7 +9,9 @@ using System.Configuration;
 
 #region 
 /*  User Manual
-    1. Выход exit
+    1. Выход - exit
+    2. Вывод всех записей - selectall
+    3. Очистка экрана - clear
  */
 #endregion
 
@@ -57,11 +59,44 @@ namespace StudentsApp1
                     }
                     #endregion
 
-                    SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
-
-                    switch (command.Split(' ')[0].ToLower())
+                    #region Clear
+                    if (command.ToLower().Equals("clear"))
                     {
+                        Console.Clear();
+                        continue;
+                    }
+                    #endregion
+
+                    SqlCommand sqlCommand = null;
+
+                    string[] commandArray = command.ToLower().Split(' ');//массив строк, на который "разрезана" строка команды по пробелу
+
+                    switch (commandArray[0])
+                    {
+                        case "selectall":
+                            sqlCommand = new SqlCommand("SELECT * FROM [Students]", sqlConnection);
+
+                            sqlDataReader = sqlCommand.ExecuteReader();
+
+                            while (sqlDataReader.Read())
+                            {
+                                Console.WriteLine($"{sqlDataReader["Id"]} {sqlDataReader["FIO"]} " +
+                                    $"{sqlDataReader["Birthday"]} {sqlDataReader["University"]} " +
+                                    $"{sqlDataReader["Group_number"]} {sqlDataReader["Course"]} " +
+                                    $"{sqlDataReader["Average_Score"]}");
+
+                                Console.WriteLine(new String('-', 30));
+                            }
+
+                            if (sqlDataReader != null)
+                            {
+                                sqlDataReader.Close();
+                            }
+
+
+                            break;
                         case "select":
+                            sqlCommand = new SqlCommand(command, sqlConnection);
 
                             sqlDataReader = sqlCommand.ExecuteReader();
 
@@ -81,18 +116,42 @@ namespace StudentsApp1
                             }
                             break;
                         case "insert":
-
+                            sqlCommand = new SqlCommand(command, sqlConnection);
                             Console.WriteLine($"Добавлено : {sqlCommand.ExecuteNonQuery()} строк(а)");
 
                             break;
                         case "update":
-
+                            sqlCommand = new SqlCommand(command, sqlConnection);
                             Console.WriteLine($"Изменено : {sqlCommand.ExecuteNonQuery()} строк(а)");
 
                             break;
                         case "delete":
-
+                            sqlCommand = new SqlCommand(command, sqlConnection);
                             Console.WriteLine($"Удалено : {sqlCommand.ExecuteNonQuery()} строк(а)");
+
+                            break;
+                        case "sortby":
+
+                            //sortby fio asc - по возрастанию
+                            sqlCommand = new SqlCommand(
+                                $"SELECT * FROM [Students] ORDER BY {commandArray[1]} {commandArray[2]}", sqlConnection);
+
+                            sqlDataReader = sqlCommand.ExecuteReader();
+
+                            while (sqlDataReader.Read())
+                            {
+                                Console.WriteLine($"{sqlDataReader["Id"]} {sqlDataReader["FIO"]} " +
+                                    $"{sqlDataReader["Birthday"]} {sqlDataReader["University"]} " +
+                                    $"{sqlDataReader["Group_number"]} {sqlDataReader["Course"]} " +
+                                    $"{sqlDataReader["Average_Score"]}");
+
+                                Console.WriteLine(new String('-', 30));
+                            }
+
+                            if (sqlDataReader != null)
+                            {
+                                sqlDataReader.Close();
+                            }
 
                             break;
                         default:
